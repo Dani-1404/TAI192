ca4b460736b7305eba9c2083067c91d3a1ab5197
Dani-1404 <018026975@upq.edu.mx>
2025-02-18 09:23:19 -0600

    Endpoint Consultar tareas

--- tareas/main.py
+++ tareas/main.py
@@ -10,7 +10,12 @@ app = FastAPI(
 
 tareas=[
         {"id": 1,"titulo":"Estudiar para el examen","Descripcion":"Repasar los apuntes de TAI ", "Vencimiento":"14-02-24", "Estado":"Completado"},
-        {"id": 2,"nombre":"Diana", "edad":23},
-        {"id": 3,"nombre":"Ivet", "edad":21},
-        {"id": 4,"nombre":"Belen", "edad":20}
+        {"id": 2,"titulo":"Estudiar para la Expocision","Descripcion":"Repasar los apuntes de diseño de interfaces ", "Vencimiento":"20-02-24", "Estado":"En proceso"},
+        {"id": 3,"titulo":"Realizar Practica","Descripcion":"Realizar Practica de Tecnologias de Virtualizacion ", "Vencimiento":"03-003-24", "Estado":"En Pendiente"},
     ]
+
+
+#Consultar tareas
+@app.get('/todostareas', tags=['Operaciones CRUD'])
+def leertareas():
+    return{"Las tareas resgistradas son":tareas}
